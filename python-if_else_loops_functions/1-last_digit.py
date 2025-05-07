b8300e862f5c9c1d562329ae24a7cd72243d9cac
gastii11 <10632@holbertonstudents.com>
2025-05-07 17:48:17 +0000

    ultimo digito y que tenga una oracion segun la condicion que cumpla

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 ultimo_digito = abs(number) % 10
 if ultimo_digito > 5:
-    print(f"Last dighit of {number} is {ultimo_digito} and is greater than 5")
+    print(f"Last digit of {number} is {ultimo_digito} and is greater than 5")
 elif ultimo_digito == 0:
-    print(f"Last dighit of {number} is {ultimo_digito} and is 0")
+    print(f"Last digit of {number} is {ultimo_digito} and is 0")
 else:
-    print(f"Last dight of {number} is {ultimo_digito} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {ultimo_digito} and is less than 6 and not 0")
