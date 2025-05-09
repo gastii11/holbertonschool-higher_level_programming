2e711b507f37b2630987430cae6c24ea599b0e68
gastii11 <10632@holbertonstudents.com>
2025-05-09 14:28:34 +0000

    agregue salto de linea

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-ultimo_digito = abs(number) % 10
+ultimo_digito = (number) % 10
 if ultimo_digito > 5:
     print(f"Last digit of {number} is {ultimo_digito} and is greater than 5")
 elif ultimo_digito == 0:
--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+eliminar_coma = ","
 for lista in range(1, 100):
     print("{:02d}".format(lista), end=", ")
-
+print()
