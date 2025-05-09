8c4bbed11dae30c6a122375096fb83ec4dcc1ccd
gastii11 <10632@holbertonstudents.com>
2025-05-09 15:10:43 +0000

    elimine la coma

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 eliminar_coma = ","
 for lista in range(1, 100):
-    print("{:02d}".format(lista), end=", ")
-print()
+    if lista == 99:
+        print(99)
+    else:
+        print("{:02d}".format(lista), end=", ")
+
