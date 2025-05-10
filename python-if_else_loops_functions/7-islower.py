9ee9b34759506a53d8b298b9b53034c5d3b80350
gastii11 <10632@holbertonstudents.com>
2025-05-10 18:48:35 +0000

    prueba2

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def islower(c):
-        if ord('c') >= 97 and ord('c') <= 122:
+        if ord(c) >= 97 and ord(c) <= 122:
             return True
         
-        elif ord('c') >= 65 and ord('c') <= 90:
+        elif ord(c) >= 65 and ord(c) <= 90:
             return False
         
         else:
