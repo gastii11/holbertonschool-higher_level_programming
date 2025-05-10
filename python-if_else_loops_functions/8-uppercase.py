3c6834371691952d256e83ed95cbf5373ed755de
gastii11 <10632@holbertonstudents.com>
2025-05-10 19:45:49 +0000

    cadena a mayusculas

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    resultado = ""
+    for caracter in str:
+        if ord(caracter)  => 97 and ord(caracter) <= 123:
+            resultado += chr(ord(caracter) - 32)
+        else:
+            resultado += caracter
+    return resultado
