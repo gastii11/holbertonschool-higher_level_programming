7213c786721d1de78340b42a4bcc00f94ac4c020
gastii11 <10632@holbertonstudents.com>
2025-05-11 16:38:09 +0000

    calculadora

---
+++ python-import_modules/1-calcuation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from calculator_1 import add
+
+if __name__ == '__main__':
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, add(a, b)))
+    print("{} * {} = {}".format(a, b, add(a, b)))
+    print("{} / {} = {}".format(a, b, add(a, b)))
