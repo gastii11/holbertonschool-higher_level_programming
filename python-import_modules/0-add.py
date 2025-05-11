5db10cad8b0f12cbea4ab82070be58dd967a67cf
gastii11 <10632@holbertonstudents.com>
2025-05-11 15:46:12 +0000

    agregue from

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-import add_0
+from add_0 import add
+
 a = 1
 b = 2
-add_0.add(a, b)
-resultado = a + b
-print("{} + {} = {}".format(a, b, resultado))
+
+
+print("{} + {} = {}".format(a, b, add(a, b)))
