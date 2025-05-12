2973d59d32a53e0b31a8864e21851f1dc8f177a8
gastii11 <10632@holbertonstudents.com>
2025-05-12 18:58:43 +0000

    lista

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,9 +2,10 @@
 
 def element_at(my_list, idx):
     largo = len(my_list)
-    if idx < 0 and idx == 0 and idx == 2 and idx == 3:
+    if idx < 0:
         return None
     elif largo > largo:
         return None
     else:
         return largo - 1
+    return my_list[idx]
