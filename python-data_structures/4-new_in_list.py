03404320e9c3386827d0ce7e38d639793776a250
gastii11 <10632@holbertonstudents.com>
2025-05-14 07:57:43 -0700

    arreglos1

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    copia = my_list.copy()
     largo = len(my_list)
     if idx < 0 or idx >= largo:
-        return my_list
-    
+        return copia
+    copia[idx] = element
+    return copia
