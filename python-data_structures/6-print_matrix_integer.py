71af76bed1a2c3d27ec07e1ef5e8219c7c13bc45
gastii11 <10632@holbertonstudents.com>
2025-05-14 07:18:24 -0700

    arreglos

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    for fila in matrix:
-        for elemnto in fila:
-            print("{:d}".format(elemnto), end=" ")
+    for row_path in matrix:
+        for n in range(len(row_path)):
+            if n < len(row_path) - 1:
+                print("{:d}".format(row_path[n]), end=" ")
+            else:
+                print("{:d}".format(row_path[n]), end="")
         print()
