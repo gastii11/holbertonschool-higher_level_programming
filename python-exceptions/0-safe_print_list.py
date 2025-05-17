5fd445fee756c7c5c92b751321764b97c9295b43
gastii11 <10632@holbertonstudents.com>
2025-05-17 22:57:57 +0000

    ejercicio 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-     nb_print = 0
-     for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            nb_print += 1
-        except (ValueError, TypeError):
-            continue
-        print()
-        return nb_print
+    elemntos_impresos = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            elemntos_impresos += 1
+    except IndexError:
+        pass
+    print()
+    return elemntos_impresos
