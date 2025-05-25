954e017d51d7a3332c7be3420838da34f9672fe9
gastii11 <10632@holbertonstudents.com>
2025-05-24 21:05:02 -0300

    agregaue un metodo estatico

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -49,6 +49,17 @@ class Rectangle:
             return 0
         return 2 * (self.widtstagramh + self.height)
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
 
     def __str__(self):
         """en forma de cadena"""
