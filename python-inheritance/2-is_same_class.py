05f83e38fe32f13b11e02bebd37504dabceed8d5
gastii11 <10632@holbertonstudents.com>
2025-05-26 16:44:40 -0300

    true and false

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,4 @@
 
 def is_same_class(obj, a_class):
     """devuelve True si el objeto es una instancia de la clase especificada"""
-    if not isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) == a_class
