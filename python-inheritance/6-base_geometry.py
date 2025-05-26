250da98163bb99502830cde575cedb52483a5640
gastii11 <10632@holbertonstudents.com>
2025-05-26 19:04:11 -0300

    aregue un error

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""clase vacia"""
+
+
+class BaseGeometry:
+    """clase vacia"""
+    def area(self):
+        """retorna un error"""
+        raise Exception("area() is not implemented")
