110ef9216197d34c8684c87aecf3470aed8e424c
gastii11 <10632@holbertonstudents.com>
2025-05-31 19:23:43 -0300

    prubea

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -9,13 +9,15 @@ class CountedIterator:
         return self
 
     def __next__(self):
+        value = next(self.iterator)
         self.count += 1
+        return value
 
-    try:
-        return next(self.iterator)
-    except StopIterable:
-        self.count -= 1
-        raise
+        try:
+            return next(self.iterator)
+        except StopIteration:
+                self.count -= 1
+                raise
 
     def get_count(self):
         return self.count
