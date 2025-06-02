9e45bf4c6b45025df3b367d4f9f22423405f6fa5
gastii11 <10632@holbertonstudents.com>
2025-06-02 14:56:26 -0300

    ejercicio 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""añade una cadena al final y devuelve numero de caracteres"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, 'a', encoding="utf-8") as file_append:
+        file_append.write(text)
+        return len(text)
