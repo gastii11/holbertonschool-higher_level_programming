59afc7fc54f7cc2b68411268d1e2b3521472b794
gastii11 <10632@holbertonstudents.com>
2025-06-02 13:37:50 -0300

    arreglos

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """archivo de texto UTF-8 y lo imprime en stdout"""
 
+
 def read_file(filename=""):
     """funcio para leer un archivo"""
     with open(filename, 'r', encoding='utf-8') as my_file_0:
