ea3445558ab6c3d8a84fdffb64d86a350ab51aaa
gastii11 <10632@holbertonstudents.com>
2025-06-02 15:40:00 -0300

    ejercicio 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""funcion que devuelve la representacion JSON"""
+
+
+def to_json_string(my_obj):
+    """devuelve representacion JSON"""
+    return json.dumps(my_obj)
