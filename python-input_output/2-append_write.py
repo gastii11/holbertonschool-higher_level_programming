cd4928f86557bb60d62920b845270a007ac8aa13
gastii11 <10632@holbertonstudents.com>
2025-06-02 15:04:25 -0300

    arreglos

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 
 
 def append_write(filename="", text=""):
+    """cadena al final y devuelve numero"""
     with open(filename, 'a', encoding="utf-8") as file_append:
         file_append.write(text)
         return len(text)
