669437134939bc92e74aa2a7fedaa7826cfc759c
gastii11 <10632@holbertonstudents.com>
2025-06-05 06:12:13 -0700

    prueba

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""añade y guarda argumentos"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """guarda un objeto json"""
+    with open(filename, 'w') as add_item:
+        json.dump(my_obj, f, indent=4)
+
+
+def load_from_json_file(filename):
+    """carga un objeto de un archivo json"""
+    with open(filename, 'r') as add_item:
+        return json.load(f)
