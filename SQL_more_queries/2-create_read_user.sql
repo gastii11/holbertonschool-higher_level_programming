1e8c59991c3853c8240e7b006f7d1568c7556306
gastii11 <10632@holbertonstudents.com>
2025-06-29 22:12:05 -0300

    ejercicio 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Creacion de base de datos y usuario
-CREATE DATABASE IF NOT EXISTS hbtn_02_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_02_2.* TO 'user_0d_2'@'localhost'
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
