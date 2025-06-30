1de715f50871ea7dd882315bbd467770472d3baa
gastii11 <10632@holbertonstudents.com>
2025-06-30 13:39:57 -0300

    ejercicio 12

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Lista todos los programas contenidos sin genero
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
