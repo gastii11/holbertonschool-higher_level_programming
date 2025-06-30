08025325df25a3e36e9a5a19522bd59326387cb6
gastii11 <10632@holbertonstudents.com>
2025-06-29 22:01:36 -0300

    ejercicio 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- Nuevo usuario con todos los privilegios
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
