7ec742edd0eb404c3f05e227c07757a5d38d9566
gastii11 <10632@holbertonstudents.com>
2025-07-02 07:26:39 -0700

    ejercicio 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Este módulo se conecta a una base de datos y lista todos los estados
+cuyo nombre comienza con N"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    result = cur.fetchall()
+
+    for i in result:
+        print(i)
+
+    cur.close()
+    db.close()
