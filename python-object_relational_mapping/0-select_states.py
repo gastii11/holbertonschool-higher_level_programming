267275e9d8198f9420e87d9874a193037454430a
gastii11 <10632@holbertonstudents.com>
2025-07-02 06:32:55 -0700

    ejercicio 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Este módulo se conecta a una base de datos MySQL y lista todos los estados"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    result = cur.fethall()
+
+    for i in result:
+        print(i)
+
+    cur.close()
+    db.close()
