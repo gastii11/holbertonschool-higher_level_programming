c18a9ce6a82f91b531701f00fa7d87c9190914ff
gastii11 <10632@holbertonstudents.com>
2025-07-02 08:03:24 -0700

    a

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Este módulo se conecta a una base de datos y lista todos los estados
+cuyo nombre comienza con N"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"
+    cur.execute(query.format(state_name))
+    result = cur.fetchall()
+
+    for i in result:
+        print(i)
+
+    cur.close()
+    db.close()
