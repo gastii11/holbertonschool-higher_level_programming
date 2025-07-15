584b46ca3ddeffc69ae42c7a6117d2d3d0114454
gastii11 <10632@holbertonstudents.com>
2025-07-15 16:32:22 -0300

    3

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleHeader = document.querySelector('#toggle_header');
+    const header = document.querySelector('header');
+
+    toggleHeader.addEventListener('click', () => {
+        if (header.classList.contains('red')) {
+            header.classList.replace('red', 'green');
+        } else {
+            header.classList.replace('green', 'red');
+        }
+    });
+});
