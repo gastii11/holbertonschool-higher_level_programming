00d146fb03f787c1205df5b1a72c8fcb37bee3a5
gastii11 <10632@holbertonstudents.com>
2025-07-15 16:46:35 -0300

    4

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+document.addEventListener('DOMContentLoaded', () => {
+    const addItem = document.querySelector('#add_item');
+
+  const myList = document.querySelector('.my_list');
+
+  addItem.addEventListener('click', () => {
+    const newItem = document.createElement('li');
+    newItem.textContent = 'Item';
+
+    myList.appendChild(newItem);
+  });
+});
