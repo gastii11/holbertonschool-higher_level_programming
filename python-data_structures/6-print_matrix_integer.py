6adf1b8e2da7b9142b05a2545df2840e76ea1868
gastii11 <10632@holbertonstudents.com>
2025-09-09 10:35:43 -0700

    ejercicio 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        print("{}".format(i))
+        for j in i:
+            print("{:d}".format(j), end="")
+        print()
