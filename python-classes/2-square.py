39d2dd0bc49ec124fb607a5234819c07fefa93f6
gastii11 <10632@holbertonstudents.com>
2025-09-15 12:12:19 -0700

    ejercicio 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """clase cuadrada"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
