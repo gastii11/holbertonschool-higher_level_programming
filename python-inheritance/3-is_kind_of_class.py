64d21ddc5c68ea7ab758cde70965c284bf2d3dcb
gastii11 <10632@holbertonstudents.com>
2025-09-22 11:52:16 -0700

    ejercicio 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""función que devuelva True si el objeto es
+una instancia de la clase especificada"""
+
+
+def is_kind_of_class(obj, a_class):
+    """función que devuelva True si el objeto es
+una instancia de la clase especificada"""
+    return isinstance(obj, a_class)
+    
+    
