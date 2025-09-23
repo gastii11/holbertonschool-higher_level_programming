1039a45361afc641e282cbac6bf340ae4e99536e
gastii11 <10632@holbertonstudents.com>
2025-09-22 20:01:33 -0700

    ejercicio 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -13,4 +13,4 @@ class Square(Rectangle):
         return self.__size * self.__size
 
     def __str__(self):
-        return f"[Square] {self.__width}/{self.__height}"
+        return f"[Square] {self.__size}/{self.__size}"
