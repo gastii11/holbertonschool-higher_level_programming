79d1abdaea75902be7636dfb8912d80922e403fe
gastii11 <10632@holbertonstudents.com>
2025-09-22 19:48:39 -0700

    ejercicio 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,6 +4,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """clase cuadrado que herda clase rectangulo"""
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
