ec9883b4033197cdd1298879c4574bbaf691270d
gastii11 <10632@holbertonstudents.com>
2025-09-22 19:55:49 -0700

    ejercicio 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""clase cuadrado"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """clase cuadrado que herda clase rectangulo"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__width}/{self.__height}"
