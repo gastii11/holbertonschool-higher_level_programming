bbae5499a2b096b36415a19759fbb07a51da0e84
gastii11 <10632@holbertonstudents.com>
2025-09-30 06:23:32 -0700

    ejercicio 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,11 +10,13 @@ def save_to_json_file(my_obj, filename):
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
 
+
 def load_from_json_file(filename):
     """crea un objeto"""
     with open(filename, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
+
 
 filename = "add_item.json"
 
