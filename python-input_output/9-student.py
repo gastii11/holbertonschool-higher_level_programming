c9dc4b8b4bed7f15447100d67894890adb6bc828
gastii11 <10632@holbertonstudents.com>
2025-09-30 06:46:11 -0700

    ejercicio 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,7 +2,7 @@
 """clase Students"""
 
 
-class Students:
+class Student:
     """clase srudents con atributos publicos"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
