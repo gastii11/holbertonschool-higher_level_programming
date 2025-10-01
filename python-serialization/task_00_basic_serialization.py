5e057f82125c75fe72c3642b28399d5aa09a5fbf
gastii11 <10632@holbertonstudents.com>
2025-10-01 06:36:49 -0700

    ejercicio 0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+import json
+
 
 def serialize_and_save_to_file(data, filename):
-    # Your code here to serialize and save data to the specified file
-    pass
+    with open(filename, "w") as f:
+        json.dump(data, f)
 
 def load_and_deserialize(filename):
-    # Your code here to load and deserialize data from the specified file
-    pass
+    with open(filename, "r") as f:
+        return json.load(f)
+
