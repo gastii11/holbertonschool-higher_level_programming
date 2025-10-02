90ba3832639acf5119a3c37089fda4d3101b1b6e
gastii11 <10632@holbertonstudents.com>
2025-10-02 06:19:43 -0700

    ejercicio 1

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import pickle
+import json
+
+class CustomObject:
+    def __init__(self, name=str, age=int, is_student=bool):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    
+    def serialize(self, filename):
+        with open('filename', 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "rb") as file:
+            obj = pickle.load(file)
+        return obj
