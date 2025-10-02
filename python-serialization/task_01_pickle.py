af01d9ac6116e9674cef7110b4fec83be8db316a
gastii11 <10632@holbertonstudents.com>
2025-10-02 06:39:15 -0700

    ejercicio 1

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -8,13 +8,11 @@ class CustomObject:
         self.age = age
         self.is_student = is_student
 
-
     def display(self):
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
-    
     def serialize(self, filename):
         try:
             with open(filename, 'wb') as f:
