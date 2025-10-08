b4d622462195d68a6a56db84e2fefe5a6788dd49
gastii11 <10632@holbertonstudents.com>
2025-10-08 07:15:10 -0700

    ejercicio 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,48 @@
+import requests
+
+response = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+print(f"Status Code: {response.status_code}")
+
+response = requests.get(url)
+if response.status_code == 200:
+    posts = response.json()
+
+    def fetch_and_print_posts():
+        response = requests.get("https://jsonplaceholder.typicode.com/posts")
+        
+        print(f"Status Code: {response.status_code}")
+
+        if response.status_code == 200:
+            posts = response.json()
+
+            for post in posts:
+                print(post["title"])
+        else:
+            print("Error al obtener los posts.")
+    
+    def fetch_and_save_posts():
+        response = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+        if response.status_code == 200:
+            posts = response.json()
+
+            filtered_posts = []
+            for post in posts:
+                filtered_posts.append({
+                    "id": post["id"],
+                    "title": post["title"],
+                    "body": post["body"]
+                })
+
+            with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
+                fieldnames = ["id", "title", "body"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+                writer.writeheader()
+
+                writer.writerows(filtered_posts)
+
+            print("Archivo 'posts.csv' creado exitosamente ✅")
+        else:
+            print(f"Error al obtener los posts. Código de estado: {response.status_code}")
