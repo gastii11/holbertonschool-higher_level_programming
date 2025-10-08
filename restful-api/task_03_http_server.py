a6362a34e89ddc239a3b9043ad5233007f379169
gastii11 <10632@holbertonstudents.com>
2025-10-08 08:41:03 -0700

    ejerciico 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -2,12 +2,31 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class Base(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
 
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
+        elif self.path == "/data":
+            import json
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"message": "This is some JSON data"}  # ejemplo de JSON
+            self.wfile.write(json.dumps(data).encode())
 
-        self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Status is OK")
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
 
 server_address = ('', 8000)
 httpd = HTTPServer(server_address, Base)
