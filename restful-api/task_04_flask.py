ea711839f7085cf9d10c3fc4375934b69653d2ad
gastii11 <10632@holbertonstudents.com>
2025-10-09 07:40:45 -0700

    ejercicio 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -26,6 +26,18 @@ def get_user(username):
         return jsonify(user)
     else:
         return jsonify({"error": "User Error"}), 404
-@app.route("/add_user", methods=['POST'])
 
-if __name__ == "__main__": app.run()
+@app.route("/add_user", methods=['POST'])
+def add_user():
+    user_data = request.json
+
+    if 'username' not in user_data:
+        return jsonify({"error": "Username is required"}), 400
+    
+    username = user_data['username']
+    users[username] = user_data
+
+    return jsonify({"message": "User added", "user": user_data}), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
