0ab70e401ef48453c5d17c8cb1cdbae631022c82
gastii11 <10632@holbertonstudents.com>
2025-10-26 18:43:59 -0700

    ejercicio 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Creacion de base de datos y tabla states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
