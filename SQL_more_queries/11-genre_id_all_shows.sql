744be1309ae00891f5589d508faa1a78bd5576de
gastii11 <10632@holbertonstudents.com>
2025-10-27 12:03:49 -0700

    ejercicio 11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Enumera todos los programas
+-- 11-all_show_genres.sql
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
