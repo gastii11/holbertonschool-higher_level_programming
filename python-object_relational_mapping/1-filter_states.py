998aabe5c0297d1f3c45f0c8a3df7debe34a020d
gastii11 <10632@holbertonstudents.com>
2025-10-28 07:09:00 -0700

    ejercicio 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Enumere todos los estados cuyo nombre comience por N"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for i in cur.fetchall():
+        print(i)
+
+    cur.close()
+    db.close()
