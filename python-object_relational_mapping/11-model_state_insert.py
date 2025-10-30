7f486cbdc2955a1dbcee25449352f3ce721b1dd2
gastii11 <10632@holbertonstudents.com>
2025-10-30 07:11:04 -0700

    ejercicio 11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Nuevo objeto State"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{user}:{password}@localhost:3306/{db_name}",
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+
+    session.close()
