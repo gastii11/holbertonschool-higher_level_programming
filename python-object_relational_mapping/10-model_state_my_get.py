e6b55a919e5a68989df095eb711e7852fb36b477
gastii11 <10632@holbertonstudents.com>
2025-10-30 07:03:43 -0700

    ejercicio 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""imprima el objeto State con el
+nombre pasado como argumento"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{user}:{password}@localhost:3306/{db_name}",
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
